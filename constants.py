0db354a1aff8e942adf717773ca5334800adfa47
alireza <alireza.es@gmail.com>
2023-11-19 12:55:19 -0700

    Add agents default user and pass in the constant file

--- constants.py
+++ constants.py
@@ -1,2 +1,14 @@
 # constants.py
+from dataclasses import dataclass
+
 DEFAULT_AUCTION_TIME_LEFT = 15  # in minutes
+
+@dataclass
+class AgentInfo:
+    jid: str
+    password: str
+
+AUCTIONEER = AgentInfo(jid="auctioneer@localhost", password="123")
+BIDDER = AgentInfo(jid="bidder@localhost", password="123")
+SELLER = AgentInfo(jid="seller@localhost", password="123")
+NOTIFICATION = AgentInfo(jid="notification@localhost", password="123")
